cf3395994a3ea0a4c448c76b6243306a6f26bee8
Arnor Ameti <arnameti@gmail.com>
2022-03-01 14:59:12 +0100

    Inharitance between Classes using Object.Create

--- 11_InharitanceBetweenClassesUsingObjectCreate.js
+++ 11_InharitanceBetweenClassesUsingObjectCreate.js
@@ -11,10 +11,31 @@ const PersonProto = {
   },
 };
 
+
+
 const steven = Object.create(PersonProto);
 
-// The StudentProto - Object, that we created earlier is now the prototype of the 
-// jay-Object. The PersonProto - Object is in turn 
+// The StudentProto - Object, that we created earlier is now the prototype of the
+// jay-Object. The PersonProto - Object is in turn the Prototype of StudentProto.
+// And so therefore, PersonProto is a parent prototype of jay, which means that it's in its
+// prototype chain.
+
 const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.fullName} and i study ${this.course}`);
+};
+
 const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
+
+
+
 
